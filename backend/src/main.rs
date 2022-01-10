e2fc75f534ffbc06a97761f9f8bb9e1708283b1d
yangshiqu <635408602@qq.com>
2022-01-10 17:23:41 +0800

    服务占位

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpServer, App, web, HttpResponse};
+
+mod dbs;
+mod gql;
+mod models;
+mod services;
+mod util;
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::to(|| HttpResponse::Ok().body("helloworld！")))
+    })
+        .bind("127.0.0.1")?
+        .run()
+        .await
 }
